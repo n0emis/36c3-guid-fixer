4513e63a7fe8c887c10ca808ce4fa26f80450174
Simeon Keske <ca-git@noemis.me>
2019-12-19 01:41:56 +0100

    add dry run

--- README.md
+++ README.md
@@ -4,4 +4,18 @@
 `pip install -r requirements.txt`
 
 ## How to use
-`WIKI_USERNAME=noemis WIKI_PASSWORD=test1234 python guid-fixer.py`
+`python guid-fixer.py -u USERNAME -p PASSWORD`
+
+## Help
+```
+$ python guid-fixer.py -h
+Usage: guid-fixer.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -u USERNAME, --username=USERNAME
+                        the wiki username (required)
+  -p PASSWORD, --password=PASSWORD
+                        the wiki password (required)
+  -d, --dry-run         perform a dry run (create no changes)
+```
--- guid-fixer.py
+++ guid-fixer.py
@@ -2,12 +2,27 @@ import re
 import os
 import uuid
 import mwclient
+import optparse
+
+
+parser = optparse.OptionParser()
+parser.add_option('-u', '--username', help="the wiki username (required)")
+parser.add_option('-p', '--password', help="the wiki password (required)")
+parser.add_option('-d', '--dry-run', action="store_true", dest='dry', help="perform a dry run (create no changes)")
+options, args = parser.parse_args()
+
+if not options.dry and (not options.username or not options.password):
+    parser.error("username and password are required, when not running in dry mode")
 
 
 ua = 'guid-fixer (User:N0emis)'
 site = mwclient.Site('events.ccc.de', path='/congress/2019/wiki/', clients_useragent=ua)
+#site = mwclient.Site('noemis.me', clients_useragent=ua)
 
-site.login(os.environ['WIKI_USERNAME'] ,os.environ['WIKI_PASSWORD'])
+if not options.dry:
+    site.login(options.username, options.password)
+else:
+    print('Performing dry run')
 
 category = site.categories['Session']
 
@@ -17,7 +32,7 @@ for page in category:
     #print(page.name)
     #print('touched',page.touched)
 
-    if page.can('edit'):
+    if page.can('edit') or options.dry:
         text = page.text()
         changed = False
         page_guids = re.findall("(\|GUID=)([\w-]*)$", text, re.M)
@@ -31,7 +46,7 @@ for page in category:
                 print("replacing GUID {} with {} on {}".format(guid[1], new_guid, page.name))
             else:
                 guids.append(guid[1])
-        if changed:
+        if changed and not options.dry:
             page.save(text, 'uniquifying session GUID', minor=True, bot=True)
     else:
         print("You are not allowed to edit the page {}. Please check your permissions...".format(page.name))
